a10baa7539f65e594510b7f54fbdb2b16c3b15c6
adrie <1750927@uab.cat>
2025-04-19 17:59:10 +0200

    renamed a file, beause of _ instead of __ for space

--- clasificacio_de__digits/digit_classification.py
+++ clasificacio_de__digits/digit_classification.py
@@ -1,5 +1,10 @@
 import numpy as np
 import os
 
+def training(directory):
+    for filename in os.listdir(directory):
+        print(filename)
+
 def classificacio_digits(train, test, k): # name_train_file, name_test_file, parameter(int)
-    pass
+    training(train)
+    
